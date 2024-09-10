5c0ab10dd0600ca0d008bfd9dacc621293b2de2d
Johnathon Hyland <johnny-hyland@hotmail.co.uk>
2024-09-10 12:38:35 +0100

    chore: Update Home.py with date range slider, temperature flags, and power visualization

--- Home.py
+++ Home.py
@@ -25,8 +25,9 @@ df.set_index("DateTime", inplace=True)  # Set as index
 meta_data = pd.read_csv("data/multiple_turbine_data/Penmanshiel_WT_static.csv")
 
 # slider to select start amd end date
-min_date = df.index.min().to_pydatetime()
 max_date = df.index.max().to_pydatetime()
+# min date is 6 months before the max date
+min_date = max_date - pd.Timedelta(days=180)
 
 # Create a date range slider using st.slider
 start_date, end_date = st.sidebar.slider(
@@ -38,27 +39,32 @@ start_date, end_date = st.sidebar.slider(
 )
 
 
-# Filter for the last week of data and where Indicator is 'Active Power'
-filtered_df = df.loc[
+power_data = df.loc[
     (df.index >= start_date)
     & (df.index <= end_date)
     & (df["Indicator"] == "Power (kW)")
 ]
-# aggregate to get the total power for each turbine for the last week
-filtered_df = filtered_df.reset_index()
-filtered_df["Date"] = filtered_df["DateTime"].dt.date
-filtered_df = (
-    filtered_df.groupby(["Date", "Turbine", "Indicator"])
+power_data = power_data.reset_index()
+power_data["Date"] = power_data["DateTime"].dt.date
+power_data = (
+    power_data.groupby(["Date", "Turbine", "Indicator"])
     .agg({"Value": "mean"})
     .reset_index()
 )
 
-power_data = filtered_df[filtered_df["Indicator"] == "Power (kW)"]
+temp_data = df.loc[
+    (df.index >= (end_date - pd.Timedelta(days=1)))
+    & (df.index <= end_date)
+    & (df["Indicator"] == "Gear oil temperature (°C)")
+]
+temp_data = temp_data.reset_index()
+# count per turbine of the number of times the temperature is above 50
+temp_data["Above 50"] = temp_data["Value"] > 50
 
 col = st.columns((2, 2), gap="medium")
 
 # Merge with coordinates from meta_data on 'Turbine' and 'Alternative Title'
-map_data = filtered_df.merge(
+map_data = power_data.merge(
     meta_data[["Alternative Title", "Latitude", "Longitude"]],
     left_on="Turbine",
     right_on="Alternative Title",
@@ -84,18 +90,40 @@ with col[1]:
         # size='Power (kW)',
         color="color",
         zoom=11.5,
-        use_container_width=True
+        use_container_width=True,
     )
+    st.markdown("#### Temperature Flags")
+    columns = st.columns(5)
+
+    for i in range(1, 6):
+        value = temp_data[temp_data["Turbine"] == f"T0{i}"]["Above 50"].sum()
+        previous = [0,0,0,0,0]
+        delta_value = value - previous[i-1]
+        
+        delta_color = "inverse" if delta_value >= 1 else "normal"
+        
+        with columns[i - 1]:  # Access each column to place the metric
+            st.metric(
+                label=f"T0{i}",
+                value=value,
+                delta=str(delta_value),
+                delta_color=delta_color,
+            )
 with col[0]:
-    st.markdown('#### Power Timeseries - Mean Daily')
-    line_chart = alt.Chart(power_data).mark_line().encode(
-        x=alt.X('Date:T', title='Time'),
-        y=alt.Y('Value:Q', title='Power (kW)'),
-        color='Turbine:N',  # Color by Turbine
-        strokeDash='Turbine:N'  # Optionally differentiate lines by dashed style
-    ).properties(
-        width=700,
-        height=400,
+    st.markdown("#### Power Timeseries - Mean Daily")
+    line_chart = (
+        alt.Chart(power_data)
+        .mark_line()
+        .encode(
+            x=alt.X("Date:T", title="Time"),
+            y=alt.Y("Value:Q", title="Power (kW)"),
+            color="Turbine:N",  # Color by Turbine
+            strokeDash="Turbine:N",  # Optionally differentiate lines by dashed style
+        )
+        .properties(
+            width=700,
+            height=400,
+        )
     )
 
     # Display the chart in Streamlit
